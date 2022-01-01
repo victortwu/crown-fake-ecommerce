5a94634c4edafd6258867947dd9200632d2751c2
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-01 13:20:01 -0800

    debugged npm issues, installed react router and tested it

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom'
+import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/hats' component={HatsPage}/>
+      </Switch>
+    
+    </div>
+  );
+}
+
+export default App;
