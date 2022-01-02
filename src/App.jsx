ee2cb35c24d30e3d419ed47b084ef4af28aba3ff
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-01 23:28:38 -0800

    created header component, basic styling, links to home and shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
