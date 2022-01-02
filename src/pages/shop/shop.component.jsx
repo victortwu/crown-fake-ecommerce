d3a7561987bc6ec0d79eab586202bdcf9ad8b57b
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-01 16:50:39 -0800

    created shop page and collection preview components, mapped and tested data

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        const { collections } = this.state
+        return(
+            <div className='shop-page'>
+                {
+                    collections.map(({id, ...otherCollectionsProps}) => {
+                        return <CollectionPreview key={id} {...otherCollectionsProps}/>
+                    })
+                }
+                
+            </div>
+           
+         
+           
+        )
+    }
+}
+
+export default ShopPage
