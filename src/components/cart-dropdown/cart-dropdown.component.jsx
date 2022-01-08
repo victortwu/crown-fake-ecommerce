6383404464e35057e31a78ab6280f780e915fbde
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-07 21:46:40 -0800

    using dispatch prop, can now close dropdown when going to checkout

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import {  toggleCartHidden } from '../../redux/cart/cart.actions.js' 
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ( { cartItems, history } ) => {
+const CartDropdown = ( { cartItems, history, dispatch } ) => {
     return(
         <div className="cart-dropdown">
             <div className="cart-items">
@@ -22,7 +23,14 @@ const CartDropdown = ( { cartItems, history } ) => {
                 )}
             </div>
                
-            <CustomButton onClick={()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton> 
+            <CustomButton onClick={()=> {
+                    history.push('/checkout')
+                    dispatch(toggleCartHidden())
+                }
+            }
+            >
+                GO TO CHECKOUT
+            </CustomButton> 
                   
         </div>
     )
