96249149defe6c914d4fe843bfa46bee8ab9e573
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-08 11:12:32 -0800

    implemented arrow right and left functionality in checkout page to increase and decrease quantity of items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,7 @@
 import { CartActionTypes } from './cart.types'
 
+// SETTING ACTION OBJECTS
+
 export const toggleCartHidden = () => {
     return {
         type: CartActionTypes.TOGGLE_CART_HIDDEN
@@ -9,6 +11,20 @@ export const toggleCartHidden = () => {
 export const addItem = item => {
     return {
         type: CartActionTypes.ADD_ITEM,
+        payload: item
+    }
+}
+
+export const removeItem = item => {
+    return {
+        type: CartActionTypes.REMOVE_ITEM,
+        payload: item
+    }
+}
+
+export const clearItemFromCart = item => {
+    return {
+        type: CartActionTypes.CLEAR_ITEM_FROM_CART,
         payload: item
     }
 }
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,9 @@
+// These are stored action type strings for cart actions all in one place
+
 export const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 }
 
