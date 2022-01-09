3f24dabbc981c15c269c621f1bd5fd40a586ea55
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-08 18:51:13 -0800

    implemented a selector function to dynamically pass in url of collection and get back, for example : hats with only hats

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react' 
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
+import { selectCollection } from '../../redux/shop/shop.selectors'
+
 import './collection.styles.scss'
-                        // match is coming from <Route />
-const CollectionPage = ( { match } ) => {
+                      // since mapStateToProps names props 'collection' and ownProps are included, you just pass in { collections } to the component  
+const CollectionPage = ( { collection } ) => {
+    console.log(collection)
     return(
-        <div className="category">
+        <div className="collection-page">
             <h2>category page</h2>
         </div>
     )
 }
+                        // match is coming from <Route />, ownProps arg passes in 'regular' props
+const mapStateToProps = (state, ownProps) => {
+    return {     // the selectCollection was written as a function returning a function
+        collection: selectCollection(ownProps.match.params.collectionId)(state)
+    }
+}
 
-export default CollectionPage
+export default connect(mapStateToProps)(CollectionPage)
