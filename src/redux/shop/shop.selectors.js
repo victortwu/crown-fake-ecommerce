09ffaf3b80e1c60b937ff4aaab6862a6d1dbf794
Victor Twu <victortwu@MacBook-Pro.lan>
2022-01-08 21:05:47 -0800

    added selectCollectionsPreview function to selectors and replaced old selector in collections-overview to convert data back into array

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,19 +1,16 @@
 import { createSelector } from "reselect"
 import memoize from 'lodash.memoize'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
 )
 
 // By wrapping this function is memoize, we're saying that whenever this function gets called and receives collectionUrlParam, I want to memoize the return of this function (in this case we return a selector). If this function gets called again with the same collectionUrlParam, don't rerun this function because we'll return the same value as last time, which we've memoized so just return the selector that's been stored.
